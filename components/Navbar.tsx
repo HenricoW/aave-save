5d260e96ca6bdedeef40bb122e7e94b16f5eb89b
HenricoW <henrico.ashley.White@gmail.com>
2021-11-02 17:53:36 +0200

    Setup chakra & added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { Button, IconButton } from "@chakra-ui/button";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Heading, HStack, Text } from "@chakra-ui/layout";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+function Navbar() {
+  const isConnected = true;
+  return (
+    <Box as="header" py={5} px={6} bg="gray.700">
+      <Box as="nav" d="flex" alignItems="center" justifyContent="space-between">
+        <HStack spacing="4">
+          <Image height="40px" width="40px" src="/aave.7a37d675.svg" />
+          <Heading>Web3 Saver</Heading>
+        </HStack>
+        <HStack spacing={isConnected ? "8" : "2"}>
+          {isConnected ? (
+            <HStack spacing="3">
+              <Image height="40px" width="40px" className={styles.accountImg} src="https://placekitten.com/100/100" />
+              <Text>0x31BE7...498Aa</Text>
+            </HStack>
+          ) : (
+            <Button variant="outline" colorScheme="twitter">
+              CONNECT
+            </Button>
+          )}
+          <Menu>
+            <MenuButton
+              aria-label="Main menu"
+              as={IconButton}
+              icon={<HamburgerIcon />}
+              variant="outline"
+              colorScheme="twitter"
+            >
+              {/* MENU */}
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Save</MenuItem>
+              <MenuItem>Swap</MenuItem>
+            </MenuList>
+          </Menu>
+        </HStack>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    config: {
+      initialColorMode: "dark",
+    },
+  });
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
