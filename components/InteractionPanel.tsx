41fb2bfd09dd3eb2c65c4ee821c8a370cb0cc432
HenricoW <henrico.ashley.White@gmail.com>
2021-11-07 17:30:41 +0200

    divided into components, reducer & cntxt for state

--- components/InteractionPanel.tsx
+++ components/InteractionPanel.tsx
@@ -1,118 +1,91 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
 import { Button } from "@chakra-ui/button";
-import { Progress } from "@chakra-ui/progress";
 import { Box, Container, HStack, Text, VStack } from "@chakra-ui/layout";
 import Image from "next/image";
-import { Dispatch, SetStateAction } from "react";
-import { tokenData, userData } from "../utils/dummyData";
+import { createContext, Dispatch, SetStateAction, useReducer } from "react";
+import { appState, getTokenData, getUserAmounts } from "../utils/dummyData";
 import InteractionInput from "./InteractionInput";
+import { collateralReducer } from "../styles/reducers/Reducers";
+import CollateralBar from "./CollateralBar";
 
 export type InteractionPanelProps = {
   setIsInteractOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-const createInput = (
-  title: string,
-  helperText: string,
-  helperAmount: string,
-  topMargin: boolean,
-  btnColor: string,
-  warnValue: number,
-  precision: number,
-  btnText = "submit"
-) => (
-  <InteractionInput
-    title={title}
-    helperText={helperText}
-    helperAmount={helperAmount}
-    topMargin={topMargin}
-    btnColor={btnColor}
-    warnValue={warnValue}
-    precision={precision}
-    btnText={btnText}
-  />
-);
+const tknData = getTokenData(appState.selectedToken);
+export const fieldVals = getUserAmounts();
+export const userTokenAmountsContext = createContext(fieldVals);
 
 function InteractionPanel({ setIsInteractOpen }: InteractionPanelProps) {
-  const decimals = tokenData[0].displayDecimals;
-  const walletAmount = (userData.wallet["wBTC"] ? userData.wallet["wBTC"] : 0).toFixed(decimals);
-  const depositAmount = (userData.deposits["wBTC"] ? userData.deposits["wBTC"] : 0).toFixed(decimals);
+  const [usrFieldState, fieldsDispatch] = useReducer(collateralReducer, fieldVals);
 
   return (
-    <Container maxW="container.md" border="1px" borderColor="gray.600" borderRadius="md" p="4" mb="5">
-      <Box d="flex" justifyContent="space-between" py={2} px={4}>
-        <HStack spacing="3">
-          <Image height="60px" width="60px" src={tokenData[0].imgUrl} />
-          <Text fontWeight="bold" fontSize="lg">
-            {tokenData[0].ticker}
-          </Text>
-        </HStack>
-        <Box d="flex" justifyContent="space-between" flexBasis="45%">
-          <VStack spacing="1">
-            <Text fontSize="xs" color="gray.400">
-              Savings APY
+    <userTokenAmountsContext.Provider value={usrFieldState}>
+      <Container maxW="container.md" border="1px" borderColor="gray.600" borderRadius="md" p="4" mb="5">
+        <Box d="flex" justifyContent="space-between" py={2} px={4}>
+          <HStack spacing="3">
+            <Image height="60px" width="60px" src={tknData.imgUrl} />
+            <Text fontWeight="bold" fontSize="lg">
+              {tknData.ticker}
             </Text>
-            <Text fontSize="lg" fontWeight="bold" color="green.300">
-              {tokenData[0].saveRate.toString()} %
-            </Text>
-          </VStack>
-          <VStack spacing="1">
-            <Text fontSize="xs" color="gray.400">
-              Borrow APY
-            </Text>
-            <Text fontSize="lg" fontWeight="bold" color="orange.300">
-              {tokenData[0].borrRate.toString()} %
-            </Text>
-          </VStack>
+          </HStack>
+          <Box d="flex" justifyContent="space-between" flexBasis="45%">
+            <VStack spacing="1">
+              <Text fontSize="xs" color="gray.400">
+                Savings APY
+              </Text>
+              <Text fontSize="lg" fontWeight="bold" color="green.300">
+                {tknData.saveRate.toString()} %
+              </Text>
+            </VStack>
+            <VStack spacing="1">
+              <Text fontSize="xs" color="gray.400">
+                Borrow APY
+              </Text>
+              <Text fontSize="lg" fontWeight="bold" color="orange.300">
+                {tknData.borrRate.toString()} %
+              </Text>
+            </VStack>
+          </Box>
         </Box>
-      </Box>
-      <Box d="flex" justifyContent="space-between" pb="2" px="5">
-        <Text fontSize="lg" textAlign="center">
-          COLLATERAL LIMIT:
-        </Text>
-        <Text fontSize="lg" textAlign="center" fontWeight="bold">
-          80 %
-        </Text>
-      </Box>
-      <Tabs align="center" colorScheme="twitter">
-        <TabList>
-          <Tab>Save</Tab>
-          <Tab>Borrow</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel className="savePanel">
-            {createInput("Deposit", "Your Wallet", walletAmount, false, "green", parseFloat(walletAmount), decimals)}
-            {createInput("Withdraw", "Available", depositAmount, true, "green", parseFloat(depositAmount), decimals)}
-          </TabPanel>
-          <TabPanel className="borrowPanel">
-            {createInput("Borrow", "Available", depositAmount, false, "orange", parseFloat(depositAmount), decimals)}
-            {createInput("Repay", "Your Wallet", walletAmount, true, "orange", parseFloat(walletAmount), decimals)}
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-      <Container d="flex" flexDir="column" maxW="container.md" pt="2">
-        <Box d="flex" alignItems="center" justifyContent="space-between">
-          <Text py="3" color="gray.500">
-            Updated Collateral:
+        <Box d="flex" justifyContent="space-between" pt="1" px="5">
+          <Text fontSize="lg" textAlign="center">
+            COLLATERAL LIMIT:
           </Text>
-          <Text py="3" fontWeight="bold">
-            68 %
+          <Text fontSize="lg" textAlign="center" fontWeight="bold">
+            80 %
           </Text>
         </Box>
-        <Progress colorScheme="green" size="lg" value={68} />
-        <Button
-          variant="outline"
-          colorScheme="red"
-          d="block"
-          mt="8"
-          mx="auto"
-          w="40"
-          onClick={() => setIsInteractOpen(false)}
-        >
-          CLOSE
-        </Button>
+        <Tabs align="center" colorScheme="twitter">
+          <TabList>
+            <Tab>Save</Tab>
+            <Tab>Borrow</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <InteractionInput panelType={"savePanel"} fieldsDispatch={fieldsDispatch} />
+            </TabPanel>
+            <TabPanel>
+              <InteractionInput panelType={"borrowPanel"} fieldsDispatch={fieldsDispatch} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+        <Container d="flex" flexDir="column" maxW="container.md" pt="2">
+          <CollateralBar />
+          <Button
+            variant="outline"
+            colorScheme="red"
+            d="block"
+            mt="8"
+            mx="auto"
+            w="40"
+            onClick={() => setIsInteractOpen(false)}
+          >
+            CLOSE
+          </Button>
+        </Container>
       </Container>
-    </Container>
+    </userTokenAmountsContext.Provider>
   );
 }
 
