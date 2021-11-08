d7df009efbbc2a362f85b14c9ab9f5ebfe8fcc48
HenricoW <henrico.ashley.White@gmail.com>
2021-11-08 16:02:12 +0200

    reducer & context integration, improved validation

--- components/InteractionInput.tsx
+++ components/InteractionInput.tsx
@@ -1,64 +1,110 @@
-import React, { useState } from "react";
+import { Dispatch, useContext, useEffect, useState } from "react";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { NumberInput, NumberInputField } from "@chakra-ui/number-input";
 import { Box, HStack, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
+import { userTokenAmountsContext } from "./InteractionPanel";
+import { appState, getInputConfig, getTokenData, inputDispatchConfig } from "../utils/dummyData";
+import NumInput from "./NumInput";
 
 type InteractionInputProps = {
-  title: string;
-  helperText: string;
-  helperAmount: string;
-  topMargin: boolean;
-  btnColor: string;
-  warnValue: number;
-  precision: number;
-  btnText: string;
+  panelType: string;
+  fieldsDispatch: Dispatch<{
+    type: string;
+    payload: number;
+  }>;
 };
 
-function InteractionInput({
-  title,
-  helperText,
-  helperAmount,
-  topMargin,
-  btnColor,
-  warnValue,
-  precision,
-  btnText,
-}: InteractionInputProps) {
-  const [inputVal, setInputVal] = useState<string>();
-  const [exceedsRange, setExceedsRange] = useState(false);
+type fieldAmtType = {
+  [pos: string]: string;
+};
 
-  const handleChange = (val: string) => {
-    setExceedsRange(false);
-    let theVal = parseFloat(val);
-    if (theVal > warnValue || theVal < 0) setExceedsRange(true);
+const btnText = "submit";
+
+function InteractionInput({ panelType, fieldsDispatch }: InteractionInputProps) {
+  const [fieldAmt, setFieldAmt] = useState<fieldAmtType>({ top: "0", bottom: "0" });
+  const [topOutOfRange, setTopOutOfRange] = useState(false);
+  const [bottomOutOfRange, setBottomOutOfRange] = useState(false);
+
+  const userAmounts = useContext(userTokenAmountsContext);
+
+  const { walletAmount, depositAmount, borrowedAmount, decimals, totalDeposits, totalLoaned } = userAmounts;
+  const { price } = getTokenData(appState.selectedToken);
+
+  useEffect(() => {
+    setFieldAmt({ top: "0", bottom: "0" });
+  }, []);
+
+  const configArgs = { panelType, totalDeposits, totalLoaned, decimals, price };
+  const fieldConfig = getInputConfig({ ...configArgs, walletAmount, depositAmount, borrowedAmount });
+
+  // input change handler
+  const handleChange = (val: string, pos: "top" | "bottom") => {
+    if (val === "") return;
+    const theVal = parseFloat(val);
+    const selektah: {
+      [pos: string]: (arg: boolean) => void;
+    } = {
+      top: (arg) => setTopOutOfRange(arg),
+      bottom: (arg) => setBottomOutOfRange(arg),
+    };
+
+    setFieldAmt({ ...fieldAmt, [pos]: val });
+
+    // validation feedback
+    const limitAmount = fieldConfig[pos].helperAmount;
+    if (theVal > parseFloat(limitAmount) || theVal < 0) {
+      selektah[pos](true);
+    } else {
+      selektah[pos](false);
+    }
+
+    const action = { type: inputDispatchConfig[panelType][pos], payload: theVal * price };
+    fieldsDispatch(action);
+  };
+
+  // input field labels
+  const inputLabels = (pos: "top" | "bottom") => {
+    return (
+      <Box d="flex" alignItems="center" justifyContent="space-between">
+        <FormLabel>{fieldConfig[pos].title}</FormLabel>
+        <Text fontSize="sm" color="gray.500">
+          {fieldConfig[pos].helperText}: {fieldConfig[pos].helperAmount}
+        </Text>
+      </Box>
+    );
   };
 
   return (
-    <HStack alignItems="end">
-      <FormControl id="withdraw" mt={topMargin ? "8" : "0"}>
-        <Box d="flex" alignItems="center" justifyContent="space-between">
-          <FormLabel>{title}</FormLabel>
-          <Text fontSize="sm" color="gray.500">
-            {helperText}: {helperAmount}
-          </Text>
-        </Box>
-        <NumberInput
-          value={inputVal}
-          onChange={(val) => handleChange(val)}
-          precision={precision}
-          keepWithinRange={false}
-          clampValueOnBlur={false}
-          isInvalid={exceedsRange}
-          focusBorderColor={exceedsRange ? "red.600" : "blue.500"}
-        >
-          <NumberInputField />
-        </NumberInput>
-      </FormControl>
-      <Button colorScheme={btnColor} variant="outline">
-        {btnText.toUpperCase()}
-      </Button>
-    </HStack>
+    <>
+      <HStack alignItems="end">
+        <FormControl id={panelType + "-top"} mt="0">
+          {inputLabels("top")}
+          <NumInput
+            value={fieldAmt.top}
+            onChange={(val) => handleChange(val, "top")}
+            precision={decimals}
+            outOfRange={topOutOfRange}
+          />
+        </FormControl>
+        <Button colorScheme={fieldConfig.btnColor} variant="outline">
+          {btnText.toUpperCase()}
+        </Button>
+      </HStack>
+      <HStack alignItems="end">
+        <FormControl id={panelType + "-bottom"} mt="6">
+          {inputLabels("bottom")}
+          <NumInput
+            value={fieldAmt.bottom}
+            onChange={(val) => handleChange(val, "bottom")}
+            precision={decimals}
+            outOfRange={bottomOutOfRange}
+          />
+        </FormControl>
+        <Button colorScheme={fieldConfig.btnColor} variant="outline">
+          {btnText.toUpperCase()}
+        </Button>
+      </HStack>
+    </>
   );
 }
 
