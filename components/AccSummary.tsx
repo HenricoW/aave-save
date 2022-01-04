2d3db3f753a36cb858f6ccc75c09b8c401431a19
HenricoW <henrico.ashley.White@gmail.com>
2022-01-04 15:06:35 +0200

    create acc logic & ui, fetch real prices & rates

--- components/AccSummary.tsx
+++ components/AccSummary.tsx
@@ -4,20 +4,56 @@ import Image from "next/image";
 import style from "../styles/AccSummary.module.css";
 import React, { useContext, useEffect } from "react";
 import { AppContext, AppDispatchContext } from "./Layout";
-import { getWalletBalances } from "../utils/utilFunctions";
+import { getTknPrices, getTokenRates, getUserDepositAmounts, getWalletBalances } from "../utils/utilFunctions";
+import { TokenContext, TokenDispatchContext } from "../pages";
+import { ZERO_ADDR } from "../utils/dummyData";
+import { Button } from "@chakra-ui/react";
+import { Signer } from "ethers";
 
 function AccSummary() {
-  const { userData, userAmounts, isUserConnected, contracts } = useContext(AppContext);
+  const { userData, userAmounts, isUserConnected, contracts, web3 } = useContext(AppContext);
   const appDispatch = useContext(AppDispatchContext);
+
+  const tokenData = useContext(TokenContext);
+  const tknDataDispatch = useContext(TokenDispatchContext);
 
   useEffect(() => {
     if (contracts) {
       (async () => {
-        const payload = await getWalletBalances(userData.address, contracts);
-        appDispatch({ type: "setWalletAmts", payload, target: "user" });
+        const suppBorrRates = await getTokenRates(contracts);
+        tknDataDispatch({ type: "setSuppRates", payload: suppBorrRates.supply });
+        tknDataDispatch({ type: "setBorrRates", payload: suppBorrRates.borrow });
+
+        if (userData.address !== ZERO_ADDR) {
+          const payload = await getWalletBalances(userData.address, contracts);
+          appDispatch({ type: "setWalletAmts", payload, target: "user" });
+        }
+
+        // await getUserDepositAmounts(userData.appWallet, contracts);
       })();
     }
-  }, [contracts]);
+  }, [contracts, userData.address]);
+
+  useEffect(() => {
+    (async () => {
+      // get token prices
+      const prices = await getTknPrices();
+      tknDataDispatch({ type: "setPrices", payload: prices });
+    })();
+  }, []);
+
+  const createAnAcc = async () => {
+    try {
+      await contracts?.w3Saver.connect(web3?.signer as Signer).createAccount();
+
+      const userAccAddr = await contracts?.["w3Saver"].getAccAddr(userData.address);
+      appDispatch({ type: "signIn", payload: { ...userData, appWallet: userAccAddr }, target: "user" });
+
+      console.log("Account created at: ", userAccAddr);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const { totalDeposits, totalLoaned } = userAmounts;
   const userCollateral = (totalLoaned / totalDeposits) * 100;
@@ -40,10 +76,13 @@ function AccSummary() {
             <Box className="accountValues" d="flex" alignItems="center">
               <Image src={userData.blockie} width="70px" height="70px" className={style.blockieImg} />
               <Box d="flex" flexDir="column" pb="2" ml="4">
-                <Text maxW="260px" isTruncated>
-                  {userData.address}
+                <Text maxW="260px" color="gray.500" isTruncated>
+                  Your Account:
                 </Text>
-                <Box d="flex" alignItems="center" justifyContent="space-between" flexGrow={1}>
+                <Text maxW="260px" isTruncated mb="3">
+                  {userData.appWallet}
+                </Text>
+                <Box d="flex" alignItems="center" justifyContent="space-between" flexGrow={1} mb="1">
                   <Text fontSize="sm" color="gray.500">
                     Total Deposits:
                   </Text>
@@ -61,14 +100,22 @@ function AccSummary() {
                 </Box>
               </Box>
             </Box>
-            <Box className="collGraph" d="flex" flexDir="column" alignItems="center" ml="4">
-              <CircularProgress size="95px" thickness="15px" value={userCollateral} color="green.400" mb="2">
-                <CircularProgressLabel>{userCollateral.toFixed(0)}%</CircularProgressLabel>
-              </CircularProgress>
-              <Text fontSize="sm" color="gray.500">
-                Collateral Rate
-              </Text>
-            </Box>
+            {userData.appWallet === ZERO_ADDR ? (
+              <Box d="flex" flexDir="column" justifyContent="center">
+                <Button variant="outline" colorScheme="twitter" onClick={createAnAcc}>
+                  CREATE AN ACCOUNT
+                </Button>
+              </Box>
+            ) : (
+              <Box className="collGraph" d="flex" flexDir="column" alignItems="center" ml="4">
+                <CircularProgress size="95px" thickness="15px" value={userCollateral} color="green.400" mb="2">
+                  <CircularProgressLabel>{userCollateral.toFixed(0)}%</CircularProgressLabel>
+                </CircularProgress>
+                <Text fontSize="sm" color="gray.500">
+                  Collateral Rate
+                </Text>
+              </Box>
+            )}
           </Box>
         </Container>
       ) : null}
